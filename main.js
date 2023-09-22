3371a8a28c7bd7c09d7f9f48f49bc8c8efd4d410
Aritro Saha <70253408+halcyon-past@users.noreply.github.com>
2023-09-23 04:45:00 +0530

    Update main.js
    Added a new url

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ setInterval(() => {
     });
 }, 300000);
 
+setInterval(() => {
+  axios
+    .get("https://eduinsight.onrender.com/")
+    .then((response) => {
+      console.log(`Pinged https://eduinsight.onrender.com - Status Code: ${response.status}`);
+    })
+    .catch((error) => {
+      console.error(`Error pinging https://eduinsight.onrender.com: ${error.message}`);
+    });
+}, 100000);
+
 // Start the Express server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
